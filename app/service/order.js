457e8af30968bbf78832ae92ebf629468ef51bd9
dong <gengyudonga@gmail.com>
2019-09-13 20:02:32 +0800

    更改订单表结构

--- app/service/order.js
+++ app/service/order.js
@@ -7,22 +7,39 @@ const md5 = require('md5');
 class OrderService extends Service {
   async update() {
     // 更新未支付订单
-    const { ctx, app, config: { orderValidity } } = this;
+    const {
+      ctx,
+      app,
+      config: { orderValidity },
+    } = this;
     const { Op } = app.Sequelize;
-    await ctx.model.Orders.update({
-      pay_status: '已过期',
-    }, {
-      where: {
-        created_at: {
-          [Op.lt]: +new Date() - orderValidity * 60 * 1000,
-        },
-        pay_status: '未支付',
+    await ctx.model.Orders.update(
+      {
+        pay_status: '已过期',
       },
-    });
+      {
+        where: {
+          created_at: {
+            [Op.lt]: +new Date() - orderValidity * 60 * 1000,
+          },
+          pay_status: '未支付',
+        },
+      }
+    );
   }
-  async createOrder(qr_url, qr_price) { // 生成支付宝订单
+  async createOrder(qr_url, qr_price) {
+    // 生成支付宝订单
     const { ctx } = this;
-    const { order_id, video_id, order_type, order_price, sign, order_name, extension, redirect_url } = ctx.request.body;
+    const {
+      order_id,
+      video_id,
+      order_type,
+      order_price,
+      sign,
+      order_name,
+      extension,
+      redirect_url,
+    } = ctx.request.body;
     return ctx.model.Orders.create({
       order_id,
       video_id,
@@ -49,8 +66,13 @@ class OrderService extends Service {
       },
     });
   }
-  async save_order(qr_price, order_type) { // 根据客户端推送来的金额和收款方式修改订单状态
-    const { ctx, app, config: { orderValidity, secretkey } } = this;
+  async save_order(qr_price, order_type) {
+    // 根据客户端推送来的金额和收款方式修改订单状态
+    const {
+      ctx,
+      app,
+      config: { orderValidity, secretkey },
+    } = this;
     const { Op } = app.Sequelize;
     try {
       const orderData = await ctx.model.Orders.findOne({
@@ -63,45 +85,77 @@ class OrderService extends Service {
           },
         },
       });
-      const result = await ctx.model.Orders.update({
-        pay_status: '已支付',
-      }, {
-        where: {
-          qr_price,
-          order_type,
-          pay_status: '未支付',
-          created_at: {
-            [Op.gt]: +new Date() - orderValidity * 60 * 1000,
-          },
+      const result = await ctx.model.Orders.update(
+        {
+          pay_status: '已支付',
         },
-      });
+        {
+          where: {
+            qr_price,
+            order_type,
+            pay_status: '未支付',
+            created_at: {
+              [Op.gt]: +new Date() - orderValidity * 60 * 1000,
+            },
+          },
+        }
+      );
       if (result[0] === 0) {
         return false;
       }
-      const { order_id, qr_price: price, extension, redirect_url } = orderData;
+      const {
+        order_id,
+        video_id,
+        qr_price: price,
+        extension,
+        redirect_url,
+      } = orderData;
       // sign md5(md5(order_id) + secretkey)
       const sign = md5(md5(order_id) + secretkey);
-      const url = redirect_url + '?order_id=' + order_id + '&qr_price=' + price + '&extension=' + extension + '&sign=' + sign;
-      await this.get_redirect_url(url);
+      const questBody = {
+        orderId: order_id,
+        videoId: video_id,
+      };
+      await this.get_redirect_url(redirect_url, questBody);
+      // const url = redirect_url + '?order_id=' + order_id  + '&qr_price=' + price + '&extension=' + extension + '&sign=' + sign;
+      // await this.get_redirect_url(url);
       return true;
     } catch (e) {
       return false;
     }
   }
-  async get_redirect_url(redirect_url) {
-    request.get(redirect_url, error => {
-      if (error) {
-        console.log('收款通知失败,请检查redirect_url是否正确!' + redirect_url);
-      } else {
+  async get_redirect_url(redirect_url, questBody) {
+    request.post(
+      redirect_url,
+      {
+        json: questBody
+      },
+      (error) => {
+        if (error) {
+          console.log('收款通知失败,请检查redirect_url是否正确!' + redirect_url);
+        }
         console.log('收款成功，已通知服务器' + redirect_url);
       }
-    });
-  }
-  async order_Pay_Status(order_id) {  // 用户查询订单支付状态
+    );
 
-    const { ctx, app, config: { orderValidity, secretkey } } = this;
+    // request.get(redirect_url, error => {
+    //   if (error) {
+    //     console.log('收款通知失败,请检查redirect_url是否正确!' + redirect_url);
+    //   } else {
+    //     console.log('收款成功，已通知服务器' + redirect_url);
+    //   }
+    // });
+  }
+  async order_Pay_Status(order_id) {
+    // 用户查询订单支付状态
+
+    const {
+      ctx,
+      app,
+      config: { orderValidity, secretkey },
+    } = this;
     const { Op } = app.Sequelize;
-    console.log('查询订单支付状态, service层', order_id)
+    console.log('查询订单支付状态, service层', order_id);
 
     // try {
     //   const orderData = await ctx.modle.Orders.findOne({
