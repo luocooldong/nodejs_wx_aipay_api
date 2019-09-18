349f09aa340c6bce3ca85a511f0c9248c64eed68
dong <gengyudonga@gmail.com>
2019-09-19 00:28:06 +0800

    init

--- app/service/order.js
+++ app/service/order.js
@@ -129,7 +129,8 @@ class OrderService extends Service {
       'User-Agent': `Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1`,
     };
 
-    request.set('User-Agent', `Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1`).post(
+    // request.set('User-Agent', `Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1`)
+    request.post(
       redirect_url,
       {
         json: questBody
